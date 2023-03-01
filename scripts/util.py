1843818e1d9a5c15db2b272f1011b07a53487148
Zachary Cline <zachcline@gmail.com>
2023-03-01 10:20:55 -0500

    Replace JSON configs with yaml; allow ints as dirs
    - Replace the config.json and homework_problems.json files with
      equivalent yaml files, to then load with PyYAML. The YAML
      serialization format is a little easier to read and allows for
      comments, unlike JSON. It also allows for easier editing of
      the homework_problems files.
    - Change the format of config files for autogenerated documents to
      an associative array with key being the title, and the values
      being the list of problems (nested appropriately to follow the
      directory structure in the problems directory)
    - Allow for ints in config files for autogenerated documents to
      make specifying chapter/section/problem numbers in a textbook
      easier (no need to wrap in quotation marks).
    - Rework the create_document.py and util.py script according to
      the changes above.

--- scripts/util.py
+++ scripts/util.py
@@ -2,65 +2,80 @@ from pathlib import Path
 
 
 def compress_directories(item, parent_dir=Path()):
-    """Returns a list of endpoint paths based on a nested dict/list/string/
-    pathlib.Path with parent_dir as the root directory"""
+    """Returns a list of endpoint paths based on a nested 
+    dict/list/string/int/pathlib.Path with parent_dir as the 
+    root directory"""
     paths = []
     if any([isinstance(item, t) for t in (str, Path)]):
         paths.append(parent_dir / item)
+    elif isinstance(item, int):
+        paths.append(parent_dir / str(item))
     elif isinstance(item, list):
         for d in item:
-            if any([isinstance(d, t) for t in (str, dict, Path)]):
+            if any([isinstance(d, t) for t in (str, int, dict, Path)]):
                 paths.extend(compress_directories(d, parent_dir))
             else:
-                raise TypeError('Nested lists are not valid directory structures')
+                raise TypeError('Nested lists are not valid directory '
+                                'structures')
     elif isinstance(item, dict):
         for a, d in item.items():
-            new_parent = parent_dir / a
-            if any([isinstance(d, t) for t in (str, list, dict, Path)]):
+            new_parent = parent_dir / str(a)
+            if any([isinstance(d, t) for t in (str, int, list, dict, Path)]):
                 paths.extend(compress_directories(d, new_parent))
             else:
-                raise TypeError('Only lists, dicts, strs, and Paths can be passed to compress_directories')
+                raise TypeError('Only lists, dicts, strs, ints, and Paths '
+                                'can be passed to compress_directories')
     else:
-        raise TypeError('Only lists, dicts, strs, and Paths can be passed to compress_directories')
+        raise TypeError('Only lists, dicts, strs, inst, and Paths can be '
+                        'passed to compress_directories')
     return paths
 
+
 def create_directories(item, parent_dir=Path()):
-    """Creates subdirectories of parent_dir based on a nested dict/list/string"""
+    """Creates subdirectories of parent_dir based on a nested 
+    dict/list/string"""
     for dir in compress_directories(item, parent_dir=parent_dir):
         try:
             Path.mkdir(dir, parents=True)
         except FileExistsError as e:
             print(f"{e} already exists and will not be replaced")
 
+
 def descend_directory(d, parent_dir=Path()):
     """Generator that yields directories descended from a nested dictionary"""
     if isinstance(d, dict):
         for x, y in d.items():
-            yield (parent_dir / x)
-            yield from descend_directory(y, parent_dir=(parent_dir / x))
+            yield (parent_dir / str(x))
+            yield from descend_directory(y, parent_dir=(parent_dir / str(x)))
     elif isinstance(d, list):
         for x in d:
             yield from descend_directory(x, parent_dir=parent_dir)
-    elif any(isinstance(d, t) for t in (str, Path)):
-        yield (parent_dir / d)
+    elif any(isinstance(d, t) for t in (str, int, Path)):
+        yield (parent_dir / str(d))
     else:
-        raise TypeError('Only lists, dicts, strs, and Paths can be passed to descend_directory')
+        raise TypeError("Only lists, dicts, strs, ints, and Paths can be passed "
+                        "to descend_directory")
+
 
 def path_to_dict(path):
     """Generate a nested dict/list/str of folder/file names showing the 
-    directory structre of path."""
+    directory structure of path."""
     if any((x.is_file() for x in path.iterdir())):
         dir_files = [x.name for x in path.iterdir() if x.is_file()]
-        dir_files.extend(({x.name: path_to_dict(x)} for x in path.iterdir() if x.is_dir()))
+        dir_files.extend(({x.name: path_to_dict(x)} for x in path.iterdir() 
+                          if x.is_dir()))
         return dir_files
     else:
         return {x.name: path_to_dict(x) for x in path.iterdir()}
 
+
 def latex_escape(inp_str):
-    ret_str = inp_str.replace('\\', '\\textbackslash')
+    """Return a version of inp_str with all the proper escaped characters
+    to be used in a LaTeX document."""
+    ret_str = inp_str.replace('\\', '\\textbackslash ')
     for chr in ('&', '%', '$', '#', '_', '{', '}'):
         ret_str = ret_str.replace(chr, f"\{chr}")
     return (ret_str
-        .replace('~', '\\textasciitilde')
-        .replace('^', '\\textasciicircum')
+        .replace('~', '\\textasciitilde ')
+        .replace('^', '\\textasciicircum ')
         )
